300481648a68a4aa3ee6f6da75db7f9dcba0be86
Bouakkaz Lotfi <61977293+BouakkazLotfiG@users.noreply.github.com>
2022-10-05 17:21:52 +0100

    discover 30% done

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,36 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux/es/exports";
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+
+const SongCard = ({ i, song }) => {
+  const activeSong = "Test";
+  return (
+    <div>
+      <div className="flex flew-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-mg cursor-pointer">
+        <div className="relative w-full h-56 group">
+          <div
+            className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+              activeSong?.title === song.title
+                ? "flex bg-black bg-opacity-70"
+                : "hidden"
+            }`}
+          >
+            <PlayPause />
+          </div>
+          <img src={song.images?.coverart} alt="song-img" />
+        </div>
+        <div className="mt-4 flex flex-col">
+          <p>
+            <Link>{song.title}</Link>
+          </p>
+          <p>
+            <Link>{song.subtitle}</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SongCard;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "e02e455f89msh17d49b65cbab3eap1fda1fjsnb96e1c455f08"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
